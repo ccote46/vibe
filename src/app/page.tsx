180f3c88d8e3de53366cd1119ee6ba365fe86c29
28crc5 <crc5@williams.edu>
2025-12-21 14:00:47 -0500

    03: trpc-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import { getQueryClient, trpc } from "@/trpc/server";
+import { HydrationBoundary, dehydrate } from "@tanstack/react-query";
+import { Client } from "./client";
+import { Suspense } from "react";
 
-const Page=  () => {
+const Page = async () => {
+  const queryClient = getQueryClient()
+  void queryClient.prefetchQuery(trpc.createAI.queryOptions({text:"Charlotte PREFETCH"}));
+  
   return (
-    <div>hi</div>
+    <HydrationBoundary state = {dehydrate(queryClient)}> 
+    <Suspense fallback={<p>Loading..</p>}>
+      <Client />
+    </Suspense>
+    </HydrationBoundary>
   );
 }
 
