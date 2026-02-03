f0c89d0e546473f70e0110c4e76429a8e47d441e
28crc5 <crc5@williams.edu>
2026-02-03 15:40:19 -0500

    homepage layout done

--- src/modules/projects/ui/components/messages-container.tsx
+++ src/modules/projects/ui/components/messages-container.tsx
@@ -19,6 +19,7 @@ export const MessagesContainer = ({
     }: Props) => {
     const bottomRef = useRef<HTMLDivElement>(null);
     const trpc = useTRPC();
+    const lastAssistantMessageIdRef = useRef<string | null>(null);
     const { data: messages } = useSuspenseQuery(trpc.messages.getMany.queryOptions({
         projectId,
     }, {
@@ -26,15 +27,18 @@ export const MessagesContainer = ({
         refetchInterval:5000,
     }));
 
-    //TODO-- causing problems
-    // useEffect(() => {
-    //     const lastAssistantMessageWithFragment = messages.findLast(
-    //         (message) => message.role === "ASSISTANT" && !!message.fragment,
-    //     );
-    //     if(lastAssistantMessageWithFragment){
-    //         setActiveFragment(lastAssistantMessageWithFragment.fragment);
-    //     }
-    // }, [messages, setActiveFragment]);
+    useEffect(() => {
+        const lastAssistantMessage = messages.findLast(
+            (message) => message.role === "ASSISTANT"
+        );
+        if(
+            lastAssistantMessage?.fragment && 
+            lastAssistantMessage.id !== lastAssistantMessageIdRef.current
+        ){
+            setActiveFragment(lastAssistantMessage.fragment);
+            lastAssistantMessageIdRef.current = lastAssistantMessage.id
+        }
+    }, [messages, setActiveFragment]);
 
     useEffect(() =>{
         bottomRef.current?.scrollIntoView();
